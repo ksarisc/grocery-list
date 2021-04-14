62f2f6c5387726f0924a09add8199062dcfcc747
ksarisc <JacobSmith.IN@gmail.com>
2021-04-13 22:07:59 -0400

    DataSession model for storing more than 1 file at a time per user

---
+++ GroceryList.Mvc/Models/DataSession.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+
+namespace GroceryList.Mvc.Models
+{
+    public class DataSession
+    {
+        public Dictionary<string, string> Files { get; set; }
+    }
+}
