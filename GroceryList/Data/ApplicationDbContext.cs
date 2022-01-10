c51e7ad34ce09ec065aadd769ed52ff38d227a0a
ksarisc <jacobsmith.in@gmail.com>
2022-01-10 12:32:56 -0500

    remove SQL server for now - need to rethink identity & don't really want/need a full database

--- GroceryList/Data/ApplicationDbContext.cs
+++ GroceryList/Data/ApplicationDbContext.cs
@@ -1,16 +1,16 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Text;
+﻿//using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+//using Microsoft.EntityFrameworkCore;
+//using System;
+//using System.Collections.Generic;
+//using System.Text;
 
-namespace GroceryList.Data
-{
-    public class ApplicationDbContext : IdentityDbContext
-    {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
-        {
-        }
-    }
-}
+//namespace GroceryList.Data
+//{
+//    public class ApplicationDbContext : IdentityDbContext
+//    {
+//        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+//            : base(options)
+//        {
+//        }
+//    }
+//}
