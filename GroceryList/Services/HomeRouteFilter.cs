69eea53bad0c21c3d121d88e298582d59618d293
ksarisc <jacobsmith.in@gmail.com>
2022-01-10 15:00:57 -0500

    start using global filter for home id instead of hard coding on each action at controller level

---
+++ GroceryList/Services/HomeRouteFilter.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GroceryList.Services
+{
+    public class HomeRouteFilter : IAsyncActionFilter
+    {
+        public const string Label = "HomeId";
+        public const string Parameter = "homeSlug";
+        public const string Route = "/{homeId}/[controller]"; // should this start with ~
+
+        public HomeRouteFilter() { }
+
+        private string GetFromModelState(ActionExecutingContext context)
+        {
+            if (context.ModelState.TryGetValue(Parameter, out var value) &&
+                value.RawValue is string homeSlug &&
+                !string.IsNullOrWhiteSpace(homeSlug))
+            {
+                return homeSlug;
+            }
+            return null;
+        } // END GetFromModelState
+
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            var home = GetFromModelState(context);
+            if (home == null)
+            {
+                home = context.RouteData.Values
+                    .FirstOrDefault(r => r.Key.Equals(Parameter, StringComparison.Ordinal))
+                    .Value as string;
+            }
+
+            // execute MVC filter pipeline
+            var resultContext = await next();
+
+            if (resultContext.Result is ViewResult view)
+            {
+                // ?? TempData ??
+                view.ViewData[Label] = home;
+            }
+        } // END OnActionExecutionAsync
+
+        public static string GetSlug(HttpContext context = null)
+        {
+            if (context?.Request?.RouteValues?.ContainsKey(Parameter) == true)
+            {
+                return context.Request.RouteValues[Parameter] as string;
+            }
+            return null;
+        }
+    }
+}
--- GroceryList/Startup.cs
+++ GroceryList/Startup.cs
@@ -2,7 +2,7 @@ using GroceryList.Data;
 using GroceryList.Models.Config;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
+//using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -35,19 +35,20 @@ namespace GroceryList
             services.AddScoped<IUserDataRepository, UserDataRepository>();
             services.AddScoped<IGroceryRepository, GroceryRepository>();
 
+            services.AddScoped<Services.HomeRouteFilter>(); // should this be Transient?
+
             //services.AddDbContext<ApplicationDbContext>(o => o.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             //services.AddDatabaseDeveloperPageExceptionFilter();
             //services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
             //    .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddRazorPages();
-            //services.AddControllersWithViews(o => o.UseGeneralRoutePrefix("{homeId}"));//api/v{version:apiVersion}
-            var mvc = services.AddMvc(); //o => o.Filters.Add(new Microsoft.AspNetCore.Mvc.Authorization.AllowAnonymousFilter()));
-
+            //.AddMvc vs .AddControllersWithViews
+            var mvc = services.AddControllersWithViews(o => o.Filters.AddService<Services.HomeRouteFilter>());
             if (currentEnv.IsDevelopment())
             {
                 mvc.AddRazorRuntimeCompilation();
             }
-        }
+        } // END ConfigureServices
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -76,6 +77,6 @@ namespace GroceryList
             {
                 endpoints.MapDefaultControllerRoute();
             });
-        }
+        } // END Configure
     }
 }
