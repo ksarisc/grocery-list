6b152f06c9bdc14fe7da6140b62e00348ec9c36f
ksarisc <jacobsmith.in@gmail.com>
2022-04-14 03:48:09 -0400

    add/remove from cart

--- GroceryList/wwwroot/js/site.js
+++ GroceryList/wwwroot/js/site.js
@@ -2,3 +2,21 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function getElement(element) {
+    if ('string' === typeof timeElem) {
+        // what about # & . (get first)
+        return document.getElementById(timeElem);
+    }
+    return element
+}
+
+function addRemoveFromCart(timeElem, checkElem) {
+    timeElem = getElement(timeElem);
+    const timeValue = timeElem.value; // as date/time?
+    if (checkElem) {
+        checkElem = getElement(checkElem);
+        checkElem.checked = true;
+        //!checkElem.checked
+    }
+    if (timeValue) timeElem.value = "";
+} // END addRemoveFromCart
