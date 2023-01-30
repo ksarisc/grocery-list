e826693687857fd47b8ea3e9f46543ea957751fb
ksarisc <jacobsmith.in@gmail.com>
2023-01-29 20:03:47 -0500

    missing lib project

--- GroceryList.Lib/Class1.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace GroceryList.Lib
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ GroceryList.Lib/Models/Home.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace GroceryList.Lib.Models
+{
+#nullable disable
+    public class Home
+    {
+        [Required]
+        [StringLength(100, MinimumLength = 4)]
+        public string Id { get; set; }
+        [StringLength(100, MinimumLength = 4)]
+        public string Title { get; set; }
+        [Required]
+        [StringLength(200, MinimumLength = 4)]
+        public string CreatedBy { get; set; }
+        [Required]
+        public DateTimeOffset CreatedTime { get; set; }
+        [Required]
+        public string CreatedByMeta { get; set; }
+    }
+#nullable enable
+}
